58424f94e5f0a24c5fdff5a8eb570bd1a0608769
Okan Atabağ <okanatabag@gmail.com>
2016-10-24 00:34:31 +0300

    todo updated

--- todo.js
+++ todo.js
@@ -15,11 +15,10 @@ function Todo() {
      var q = con.query('insert into todo_list set ?', todo, function(err, result) {
       con.release();
       console.log(err);
-      console.log(q.sql);
        if (err) {
          res.send({status: 1, message: 'TODO creation failed'});
        } else {
-         res.send({status: 0, message: 'TODO created successfully'});
+         res.send({status: 0, message: 'TODO created successfully', result:result});
        }
      });
    });
